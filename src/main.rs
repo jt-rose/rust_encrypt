d731dc486c1cf0dfe77726cfbe1f6e748f3a6ff4
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-08-04 23:42:25 -0400

    get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input the string you'd like to encrypt: ");
+
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("could not read user input");
+
+    println!("Your encrypted string is: {}", user_input);
 }
